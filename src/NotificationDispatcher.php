09b51b7aa9f859a64620e93f1bf867c15255d061
Illya B. <34226834+skirkru@users.noreply.github.com>
2025-06-12 06:58:42 +0300

    Chapter 1: Dependency Injection and the beginning of competence.

---
+++ src/NotificationDispatcher.php
@@ -0,0 +1,90 @@
+<?php
+
+class User {
+    
+    const NOTIFICATION_PREFER_EMAIL = 1;
+    const NOTIFICATION_PREFER_SMS = 2;
+    const NOTIFICATION_PREFER_WHATSAPP = 3;
+
+    private string $name = "";
+    private string $email = "";
+    private int $phone = 0;
+    private $notificationPreference = self::NOTIFICATION_PREFER_EMAIL;
+
+    public function __construct(string $name, string $email, int $phone)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->phone = $phone;
+    }
+
+    public function setNotificationPreference($preference): void
+    {
+        $this->notificationPreference = $preference;
+    }
+
+    public function getNotificationPreference(): int
+    {
+        return $this->notificationPreference;
+    }
+}
+
+interface INotificationHandler 
+{
+    public function sendNotification(User $user, string $message): void;
+}
+
+class EmailSender implements INotificationHandler 
+{
+    public function sendNotification(User $user, string $message): void 
+    {
+        // Pretend logic here
+    }
+}
+
+class SMSSender implements INotificationHandler 
+{
+    public function sendNotification(User $user, string $message): void 
+    {
+        // Pretend logic here
+
+    }
+}
+class WhatsAppSender implements INotificationHandler 
+{
+    public function sendNotification(User $user, string $message): void 
+    {
+        // Pretend logic here
+
+    }
+}
+
+// This guy job is to send yapping through channel that already defined
+class NotificationDispatcher {
+
+    private array $notificationHandlers;
+
+    public function __construct(array $handlers)
+    {
+        // set the channel
+        $this->notificationHandlers = $handlers;
+    }
+
+    public function doNotify(User $user, string $message): void 
+    {
+        // get user preference
+        $preference = $user->getNotificationPreference();
+
+        // check if channel is exist to use
+        // as preference is int, it doesnt show in the error
+        if (! $this->notificationHandlers[$preference]) throw new \Exception("User Notification Preference was Invalid");
+
+        // send the message
+        $this->notificationHandlers[$preference]->sendNotification($user, $message);
+        
+    }
+
+}
+
+
+?>
---
+++ tests/NotificationTest.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+include './src/NotificationDispatcher.php';
+final class NotificationTest extends TestCase
+{
+    public function testSendFromEmail(): void
+    {
+        
+        $user = new User("User", "user@bogdan.com", 74361892221);
+        
+        $mockEmailHandler = $this->createMock(INotificationHandler::class);
+        $mockSmsHandler = $this->createMock(INotificationHandler::class);
+
+        $mockEmailHandler->expects($this->once())
+                         ->method('sendNotification');
+                         
+        $mockSmsHandler->expects($this->never())
+                       ->method('sendNotification');
+
+
+        $user->setNotificationPreference(User::NOTIFICATION_PREFER_EMAIL);
+
+        $notifier = new NotificationDispatcher([
+            User::NOTIFICATION_PREFER_EMAIL => $mockEmailHandler,
+            User::NOTIFICATION_PREFER_SMS => $mockSmsHandler,
+        ]);
+
+        $notifier->doNotify($user, "User");
+
+    }
+}
+
